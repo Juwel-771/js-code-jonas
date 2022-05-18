f2110f44fda08ed31b0896c96dc97300677228d1
Juwel-771 <juwelah99@gmail.com>
2022-05-18 16:06:37 +0600

    Code Added

--- destruc1.js
+++ destruc1.js
@@ -1,57 +1,58 @@
 // Basic Programming
-// const arr = [3, 5, 23, 13];
+const arr = [3, 5, 23, 13];
 
-// [a, b, c] = arr;
-// console.log([a, b, c]);
+[a, b, c] = arr;
+console.log([a, b, c]);
 
-// // Variable Switching
-// let arr2 = [4, 5, 3];
-// [a, b] = arr2;
-// console.log('a= '+a,'b= '+b);
-// [a, b] = [b, a];
-// console.log('a= '+a, 'b= '+b);
+// Variable Switching
+let arr2 = [4, 5, 3];
+[a, b] = arr2;
+console.log('a= '+a,'b= '+b);
 
-// // Nesting inside loop
-// let arr3 = [2, 4, 5, [10, 13, 14]];
-// [a,b, ,[z,r]]=arr3;
-// console.log([a, b, [z, r]]);
+[a, b] = [b, a];
+console.log('a= '+a, 'b= '+b);
 
-// // Rest Type
-// let arr4 = [3, 5, 23, 13, 15, 19, 25];
-// [a, b, ...rest] = arr4;
-// console.log(rest);
+// Nesting inside loop
+let arr3 = [2, 4, 5, [10, 13, 14]];
+[a,b, ,[z,r]]=arr3;
+console.log([a, b, [z, r]]);
+
+// Rest Type
+let arr4 = [3, 5, 23, 13, 15, 19, 25];
+[a, b, ...rest] = arr4;
+console.log(rest);
 
 // separate variable declaration 
-// let a,b;
-// const arr5 = [4, 3, 5];
-// [a,b] = arr5;
-// console.log(a,b);
+let a1,b1;
+const arr5 = [4, 3, 5];
+[a1,b1] = arr5;
+console.log(a1,b1);
 
 // Default Value
-// let a, b;
-// [a=5, b=2] = [1];
-// console.log(a, b);
+let a2, b2;
+[a2=5, b2=2] = [1];
+console.log(a2, b2);
 
 // Function Variable
-// function f(){
-//     return [3,5];
-// }
+function f(){
+    return [3,5];
+}
 
-// [a, b] = f();
-// console.log(a);
-// console.log(b);
+[a3, b3] = f();
+console.log(a3);
+console.log(b3);
 
 // Ignoring Return Value
-// function f(){
-//     return [4, 5, 9];
-// }
-// [a, ,g] = f();
-// console.log(a, g);
+function f(){
+    return [4, 5, 9];
+}
+[a4, ,g] = f();
+console.log(a4, g);
 
-// const[c]= f();
-// console.log(c);
+const[c]= f();
+console.log(c);
 
 // Assigning the rest of an array to a variable
 const arr6 = [1,2,3,4,5,6];
-[a,...b] = arr6;
-console.log(b);
+[a5,...b5] = arr6;
+console.log(b5);
---
+++ objDestrc.js
@@ -0,0 +1,41 @@
+// Basic Syntax.
+// const user = {
+//     id: 21,
+//     isVerified: true
+// };
+
+// const {id, isVerified} = user;
+// console.log(id, isVerified);
+
+// Assigning Variable
+// let a,b;
+// ({a,b}={a:1, b:2});
+// console.log(a,b);
+
+// Assigning new variable 
+// const obj = {p:42, q:true};
+// const {p: foo, q: bar} = obj;
+
+// console.log(foo);
+// console.log(bar);
+
+// Default Value Assign
+// const obj = {
+//     name: 'juwel',
+//     id: 382,
+//     dpt: "cse"
+// }
+
+// const {name= 1, id= 2, dpt= 3, d= 4} = obj;
+// console.log(name, id, dpt, d);
+
+// Assigning to new variable names and providing default values
+
+// const obj = {
+//     firstName: 'Md',
+//     lastName: 'Juwel',
+//     id: 382,
+//     dept: 'CSE'
+// }
+
+// const {firstName:aa}
