d8f3173192a89a9a3aba594634ecae71246bcfae
Juwel-771 <juwelah99@gmail.com>
2022-05-23 01:42:44 +0600

    AND operator Created

--- shortC.js
+++ shortC.js
@@ -16,3 +16,11 @@ const guest = obj.numberGuest || 10;
 console.log(guest);
 
 console.log(obj);
+
+
+console.log("------- AND --------");
+
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && 'jonas');
