52de57cca59500f01702cf291213ac834d774c22
Juwel-771 <juwelah99@gmail.com>
2022-05-22 13:51:46 +0600

    1st Module Practiced

--- pracRest.js
+++ pracRest.js
@@ -1,3 +1,21 @@
-const x = [1,2,3,4,5,6];
-const [a,b,c,...rest] = x;
-console.log(a,b,c,rest);
+
+// // >>>>>>>>>>>>>>>>>>> Basic <<<<<<<<<<<<<<<
+// const x = [1,2,3,4,5,6];
+// const [a,b,c,...rest] = x;
+// console.log(a,b,c,rest); // 1
+
+// const y = [3,5,6,8];
+// const [a,...rest] = y;
+// console.log(rest); // 2
+
+// const basic = ['b', 'a', 's', 'i', 'c'];
+// const [a, b, ...rest] = basic;
+// console.log(a,b,rest); // 3
+
+// const mern = ['m','e','r','n','d','v','e','l','o','p','e','r'];
+// const [a, b, c, d, ...rest] = mern;
+// console.log(a, b, c, d, rest); // 4
+
+// const react = ['R','E','A','C','T'];
+// const [r, e, ,t, ...other] = react;
+// console.log(r,e,t,other); // 5
