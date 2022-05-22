c984d443b2d76b5023e434cc27200505d14aed0e
Juwel-771 <juwelah99@gmail.com>
2022-05-22 14:05:35 +0600

    3rd module added

--- pracRest.js
+++ pracRest.js
@@ -40,3 +40,7 @@
 // const myArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 // const [ o, , d, , r, ,  f, , g, ...rest] = myArray;
 // console.log(o,d,r,f,g,rest); // 10
+
+// const arr = [1,2,3,4,5];
+// const [a,b, ...other] = arr;
+// console.log(a,b,other); // 11
