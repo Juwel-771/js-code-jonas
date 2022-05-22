77ac7bf75802f5f61aa201aac231201041748eb3
Juwel-771 <juwelah99@gmail.com>
2022-05-22 14:02:55 +0600

    2nd Module Finished

--- pracRest.js
+++ pracRest.js
@@ -24,3 +24,19 @@
 // const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // const [a, b, ,c, d, , e, ...rest] = arr;
 // console.log(a, b, c ,d ,e, rest); // 6
+
+// const arr2 = [9,4,2,5,56,234];
+// const [a1, ...rest] = arr2;
+// console.log(a1,rest); // 7
+
+// const arr = [1,2,3,5,6,6];
+// const [a,v,x,...rest] = arr;
+// console.log(a,v,x, rest); // 8
+
+// const myArray = [1,2,3,4,5,6,7,8,9,10];
+// const [ , e, ,v, ,r, ,n, ...rest] = myArray;
+// console.log(e,v,r,n,rest); // 9
+
+// const myArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+// const [ o, , d, , r, ,  f, , g, ...rest] = myArray;
+// console.log(o,d,r,f,g,rest); // 10
