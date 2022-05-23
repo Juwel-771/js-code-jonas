5531e853c242725d11aa45e8b6235d36f42a15eb
Juwel-771 <juwelah99@gmail.com>
2022-05-24 03:37:45 +0600

    Firs 1st module complted

--- Set.js
+++ Set.js
@@ -1,2 +1,21 @@
 // Creating new set
-const mySet = new Set(["a","b","c","d"]);
+// const mySet = new Set(["a","b","c","d"]);
+// console.log(mySet); // Do that 15 time
+
+// const mySet1 = new Set([1,2,3,4,5,6]);
+// console.log(mySet1); // 1
+
+// const mySet2 = new Set([1,2,3,4,5,6]);
+// console.log(mySet2); // 2
+
+// const frndsName = new Set(['Nahid', 'Amin', 'Rifat','Riyad','Munna']);
+// console.log(frndsName); // 3
+
+// const club = new Set(['Barcelona', 'Real Madrid', 'Chelsea', 'Dortmund']);
+// console.log(club); // 4
+
+// const film = new Set(['Film1','Film1','Film2','Film2']);
+// console.log(film); // 5
+
+// Note: Set only take unique value. And set mainly use for filter
+// array. Means remove duplicate value from array.
