971b4d312d282c40e85cdf77c0df565ef677ddbe
Juwel-771 <juwelah99@gmail.com>
2022-05-24 17:49:16 +0600

    String Added

---
+++ string.js
@@ -0,0 +1,28 @@
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+// taking value by index number
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log(plane[3]);
+
+// string length
+console.log(airline.length);
+console.log(plane.length);
+
+// string method
+console.log("AIR: " + airline.indexOf('Air'));
+console.log("Last:" + airline.lastIndexOf('r'));
+console.log("index: " + airline.indexOf('r'));
+
+// slide 
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ')+1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
