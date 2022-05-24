0b0ea4cd3a5d313b578b6156bb868d8cecfaca48
Juwel-771 <juwelah99@gmail.com>
2022-05-24 13:39:23 +0600

    1st module added

--- mapPrac.js
+++ mapPrac.js
@@ -1,6 +1,36 @@
-const myMap = new Map([
-    ['name', 'Juwel'],
-    ['id', 382],
-    ['age', 22]
-]);
-console.log(myMap); 
+// const myMap = new Map([
+//     ['name', 'Juwel'],
+//     ['id', 382],
+//     ['age', 22]
+// ]);
+// console.log(myMap);  // 1
+
+// const myMap = new Map([
+//     [1, 'One'],
+//     [2, 'Two'],
+//     [3, 'Three'],
+//     [4, 'Four'],
+//     [5, 'Five']
+// ]);
+
+// console.log(myMap); // 2
+
+// const mySet = new Set([1,2,3,4]);
+// mySet.add(5);
+// console.log(mySet); // 3
+
+// const myNumber  = new Map([
+//     ['one', 1],
+//     ['two', 2],
+//     ['three', 3],
+//     ['four', 4]
+// ]);
+
+// console.log(myNumber); // 4
+
+// const myText = new Map([
+//     [1, 'One'],
+//     [2, 'Two'],
+//     [3, 'Three']
+// ]);
+// console.log(myText); // 5
