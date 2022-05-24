d4ff2f46bbce24deb1134392121592c17b70cc51
Juwel-771 <juwelah99@gmail.com>
2022-05-24 16:30:40 +0600

    4th Challenge Comleted

--- CodingC3.js
+++ CodingC3.js
@@ -26,3 +26,10 @@ const time = [...gameEvents.keys()].pop();
 console.log(time);
 
 console.log(`An event happened, on average, every ${time / gameEvents.size} minutes`);
+
+// 4. Events Occurrence 
+
+for(const [min, event] of gameEvents){
+    const half = min <= 45 ? 'FIRST': 'SECOND';
+    console.log(`[${half} HALF] ${min} : ${event}`);
+}
