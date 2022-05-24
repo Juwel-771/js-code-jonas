aac9d92d7cc2ec08ddc64e00e53f5f40dbb52a21
Juwel-771 <juwelah99@gmail.com>
2022-05-24 12:43:09 +0600

    Map file structured

---
+++ maps.js
@@ -0,0 +1,30 @@
+// setting values
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest.set('categories',['Italian', 'Pizzeria','Vegetarian','Organic'])
+    .set('open', 11)
+    .set('close',21)
+    .set(true,'We are open :D')
+    .set(false,'We are close :(');
+
+    console.log(rest);
+
+// getting values
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+// checking values
+console.log(rest.has('categories'));
+
+// deleting  values
+rest.delete(2);
+console.log(rest);
+
+// clearing all values 
+// rest.clear();
