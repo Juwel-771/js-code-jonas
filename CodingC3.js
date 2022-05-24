5313bc0e5c9e8da4282cf98937509e0158c014bb
Juwel-771 <juwelah99@gmail.com>
2022-05-24 16:07:31 +0600

    Coding Challenge 3 Created

---
+++ CodingC3.js
@@ -0,0 +1,13 @@
+const gameEvents = new Map([
+    [17, 'GOAL'],
+    [36, 'Substitution'],
+    [47, 'GOAL'],
+    [61, 'Substitution'],
+    [64, 'Yellow Card'],
+    [69, 'Red Card'],
+    [70, 'Substitution'],
+    [72, 'Substitution'],
+    [76, 'GOAL'],
+    [86, 'GOAL'],
+    [92, 'Yellow Card']
+]);
