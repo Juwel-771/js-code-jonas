d4dba16b2c08e52f349c22cc5f5f88afcaa2e2ba
Juwel-771 <juwelah99@gmail.com>
2022-05-25 12:43:35 +0600

    1st module added

--- Data Structure Practice/destrucArray.js
+++ Data Structure Practice/destrucArray.js
@@ -1,7 +1,12 @@
 // Basic Syntax to write
+
 // const col  = ['red','blue','purple','yellow'];
 // const [one, two, three] = col;
 // console.log(one);
 // console.log(two);
 // console.log(three); // 1
+
+// const color = ['red', 'blue', 'green','purple'];
+// const [one, two] = color;
+// console.log(one, two); // 2
 
