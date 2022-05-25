de1213684a0073a3e667ef9e24fb28ec4f67f9b0
Juwel-771 <juwelah99@gmail.com>
2022-05-25 12:55:42 +0600

    1st module completed

--- Data Structure Practice/destrucArray.js
+++ Data Structure Practice/destrucArray.js
@@ -10,3 +10,14 @@
 // const [one, two] = color;
 // console.log(one, two); // 2
 
+// const number = [1,2,3,4,5,6,7,8,9,10];
+// const [one, two, three, four] = number;
+// console.log(one, two , three, four); // 3
+
+// const playerName = ['Messi', 'Ronaldo', 'Ozil', 'Roberto', 'Bayern', 'City'];
+// const [barca, madrid, madrid2, barca2] = playerName;
+// console.log(barca, barca2, madrid, madrid2); // 4
+
+// const playerNumber = [10, 7, 11, 21, 32];
+// const [messi, ronaldo, ozil] = playerNumber;
+// console.log(messi, ronaldo, ozil); // 5
