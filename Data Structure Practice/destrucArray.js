027037472d306729c79ddfa6e17acb504c8eb217
Juwel-771 <juwelah99@gmail.com>
2022-05-25 12:39:18 +0600

    Array Destructor started

---
+++ Data Structure Practice/destrucArray.js
@@ -0,0 +1,7 @@
+// Basic Syntax to write
+// const col  = ['red','blue','purple','yellow'];
+// const [one, two, three] = col;
+// console.log(one);
+// console.log(two);
+// console.log(three); // 1
+
