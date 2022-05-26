74bcd6ff6cc5db1c3368d3e02248139f0e3e0d77
Juwel-771 <juwelah99@gmail.com>
2022-05-26 17:34:59 +0600

    First Module Completed

--- Data Structure Practice/spread.js
+++ Data Structure Practice/spread.js
@@ -1,4 +1,26 @@
-const arr = [1,2,3,4,5,6];
-const newArr = [1,23,...arr];
-console.log(newArr);
-console.log(...newArr);
+// BASIC SYNTAX 15 TIMES.
+
+// const arr = [1,2,3,4,5,6];
+// const newArr = [1,23,...arr];
+// console.log(newArr);
+// console.log(...newArr); // 1
+
+// const arr = [1,2,3,'juwel'];
+// const newArr = [...arr, 'ahmed'];
+// console.log(newArr);
+// console.log(...newArr); // 2
+
+// const myArr = ['md','juwel','ahmed'];
+// const newArr = [...myArr, 3, 4, 6, 8];
+// console.log(newArr);
+// console.log(...newArr); // 3
+
+// const myArr = [1,2,3,4,5,6,7,8];
+// const newArr = [0,0,0,...myArr];
+// console.log(newArr);
+// console.log(...newArr); // 4
+
+const newArr = ['Md','Juwel','Ahmed'];
+const myArr = [...newArr, 'where'];
+console.log(myArr);
+console.log(...myArr);
