402966b974aaecf72e0d184d01d2407b763f183a
Juwel-771 <juwelah99@gmail.com>
2022-05-27 00:48:38 +0600

    Code Added

--- Data Structure Practice/spreadObj.js
+++ Data Structure Practice/spreadObj.js
@@ -16,14 +16,43 @@
 // console.log(merge);
 
 // Clone an Object: == 15
-const circle = {
-    ab: "AB",
-    cd: "CD"
-}
+// const circle = {
+//     ab: "AB",
+//     cd: "CD"
+// }
 
-const cloneCircle = {...circle};
-console.log(cloneCircle); 
+// const cloneCircle = {...circle};
+// console.log(cloneCircle);  
 
-// Adding object data to clone variable
-const addObj = {...cloneCircle, styles: 'red'};
-console.log(addObj);
+// Adding object data to clone variable: == 10 times
+// const addObj = {...cloneCircle, styles: 'red'};
+// console.log(addObj);
+
+// Merging Objects: == 15.
+// const circle = {
+//     radius : 10,
+//     angle: 15 
+// };
+
+// const triangle = {
+//     tr: 20,
+//     ms: 14
+// };
+
+// const merge = {...circle, ...triangle};
+
+// console.log(merge);
+
+const blueSquare = {
+    length: 100,
+    color: 'blue'
+};
+
+Object.defineProperty(blueSquare, 'color', {
+    value: 'blue',
+    enumerable: true,
+    writable: false
+
+});
+
+console.log(blueSquare);
