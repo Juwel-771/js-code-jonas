cc43ba5f6ab18125c1b0f4b502c12fc16271f185
Juwel-771 <juwelah99@gmail.com>
2022-05-28 04:01:19 +0600

    Default value function created

--- 10. Closer look to function/defaultFunction.js
+++ 10. Closer look to function/defaultFunction.js
@@ -1,16 +1,45 @@
-const bookings = [];
+// const bookings = [];
 
-const createBooking = function(flightName, passengerNumber=1, price=200){
-    const booking = {
-        flightName,
-        passengerNumber,
-        price
-    }
+// const createBooking = function(flightName, passengerNumber=1, price=200){
+//     const booking = {
+//         flightName,
+//         passengerNumber,
+//         price
+//     }
 
-    console.log(booking);
-    bookings.push(booking);
-}
+//     console.log(booking);
+//     bookings.push(booking);
+// }
 
-createBooking("20LH");
+// createBooking("20LH");
 
 //NOTE: By defining value in function parameter in ES6 way.
+
+let flight = "L23H";
+let check ={
+    name: "Juwel Ahmed",
+    passport: 234032483432
+};
+
+const crateFlight = function(flightName, passenger){
+    flightName = "L342";
+    passenger.name = "Mr "+ passenger.name;
+
+    if(passenger.passport == 234032483432){
+        alert('Check In');
+    }else{
+        alert('Wrong Passport');
+    }
+}
+
+crateFlight(flight,check);
+console.log(flight);
+console.log(check);
+
+const catchFault = function(pss){
+    pss.passport = Math.trunc(Math.random()*10000000);
+    console.log(pss);
+}
+
+catchFault(check);
+crateFlight(flight,check);
