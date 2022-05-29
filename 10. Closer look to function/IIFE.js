d2465b31f5afee230b11319def4fb3a3074fe286
Juwel-771 <juwelah99@gmail.com>
2022-05-29 23:22:28 +0600

    Private data stored

--- 10. Closer look to function/IIFE.js
+++ 10. Closer look to function/IIFE.js
@@ -1,3 +1,13 @@
-(function(){
-    console.log("This will call immediately");
-}());
+// (function(){
+//     console.log("This will call immediately");
+// })();
+
+
+// {
+//     // const privateData = "This is private section"; doesn't work
+//     var privateData = "Thi is also private data"; // doesn't work
+// }
+
+
+var privateData = "This is private data";
+console.log(privateData);
