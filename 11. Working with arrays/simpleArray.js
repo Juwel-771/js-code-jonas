2d6f81c7aa8591a570ec098d672ae94b7949497a
Juwel-771 <juwelah99@gmail.com>
2022-05-30 12:00:53 +0600

    Section 11 added

---
+++ 11. Working with arrays/simpleArray.js
@@ -0,0 +1,10 @@
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE Method
+console.log(arr.slice(2));
+console.log(arr.slice(1,3));
+console.log(arr.slice(2,4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1,-2));
+console.log(arr.slice()); // Copying Array
