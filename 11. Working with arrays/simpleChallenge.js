a40c6b7dca52b79de8c913f419280057044fd003
Juwel-771 <juwelah99@gmail.com>
2022-05-30 21:01:19 +0600

    My logic added

--- 11. Working with arrays/simpleChallenge.js
+++ 11. Working with arrays/simpleChallenge.js
@@ -1,7 +1,13 @@
 
 const date = '2022-5-5';
 
-const [year, month, day] = date.split('-');
+// const [year, month, day] = date.split('-');
 
-const result = [month, day, year].join('/');
-console.log(result);
+// const result = [month, day, year].join('/');
+// console.log(result);
+
+const newDate = date.split('-');
+console.log(newDate);
+const dateManage = newDate.reverse();
+const finaleDate = dateManage.join('/');
+console.log(finaleDate);
