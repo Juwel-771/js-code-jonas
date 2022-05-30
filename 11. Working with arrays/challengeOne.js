9a5dd1a4c8b0b7ea858b9b708a8368025dfc4b29
Juwel-771 <juwelah99@gmail.com>
2022-05-30 20:35:21 +0600

    Challenge 2 added

--- 11. Working with arrays/challengeOne.js
+++ 11. Working with arrays/challengeOne.js
@@ -5,6 +5,14 @@ const checkDogs = function(dogsJulia, dogsKate){
 
     const dogs = dogsJuliaCorrect.concat(dogsKate);
     console.log(dogs);
+
+    dogs.forEach(function(dog,i){
+        if(dog >= 3){
+            console.log(`Dog number ${i+1} is an adult and is ${dog} years old`)
+        }else{
+            console.log(`Dog number ${i+1} is still a puppy`);
+        }
+    });
 };
 
 checkDogs([3,5,2,12,7],[4,1,15,8,3]);
