a4f0a365935b03fddd4677e4dfbe483072700ab1
Juwel-771 <juwelah99@gmail.com>
2022-05-30 12:14:31 +0600

    Splice method added

--- 11. Working with arrays/simpleArray.js
+++ 11. Working with arrays/simpleArray.js
@@ -8,3 +8,12 @@ console.log(arr.slice(-2));
 console.log(arr.slice(-1));
 console.log(arr.slice(1,-2));
 console.log(arr.slice()); // Copying Array
+
+// SPLICE Method - this method make effect on ORIGINAL ARRAY.
+
+// console.log(arr.splice(2,4));
+// arr.splice(-1);
+// console.log(arr);
+// console.log(arr.splice(1,2));
+// console.log(arr.splice(1,3));
+// console.log(arr.splice(1,5));
