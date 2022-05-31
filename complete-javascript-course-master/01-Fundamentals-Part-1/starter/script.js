c8e91796c1a8f542122301cd1258b478f5bda829
Juwel-771 <juwelah99@gmail.com>
2022-05-31 12:16:36 +0600

    File structured

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++
@@ -1,6 +0,0 @@
-let js = "amazing";
-if(js==="amazing"){
-  alert("JavaScript is FUN!!!");
-}
-
-console.log(45+10+96+30-10);
--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script12.js
+++
@@ -1,9 +0,0 @@
-const firstName = "Juwel";
-const birthDate = 1999;
-const year = 2022;
-const job = "Full Stack Web Developer";
-const passion = "Coding and Marketing";
-
-const mySelf = `Hi my name is ${firstName} ⚽. I'm ${year - birthDate} years old. My job is ${job}
- and my passion is ${passion}.`;
-console.log(mySelf);
