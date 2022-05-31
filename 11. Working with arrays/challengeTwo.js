316e5cea4d6fefc94e74b4e73e76e4863fb9b7b2
Juwel-771 <juwelah99@gmail.com>
2022-05-31 11:51:16 +0600

    challenge two added

--- 11. Working with arrays/challengeTwo.js
+++ 11. Working with arrays/challengeTwo.js
@@ -2,6 +2,9 @@
 const calcAverageHumanAge = function(ages){
     const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
     console.log(humanAges);
+
+    const adults = humanAges.filter(age => age >= 18);
+    console.log(adults);
 };
 
 calcAverageHumanAge([5,2,4,1,15,8,3]);
