6d16ec76def8b78707a47ae905de1560dc3eb863
Juwel-771 <juwelah99@gmail.com>
2022-05-31 19:06:56 +0600

    Concat method added

--- 11. Working with arrays/arrayMethodOne.js
+++ 11. Working with arrays/arrayMethodOne.js
@@ -11,3 +11,40 @@
 // const newOBj = arr1.concat(arr2);
 // console.log(newOBj); // 2
 
+// const name = [1,34,45,5,245];
+// const numer = [23,34,521];
+// console.log(name.concat(numer)); // 3
+
+// const skill = ['M','E','R','N'];
+// const skillStack= ['MO','EX','RC','ND'];
+// const newStack = skill.concat(skillStack);
+// console.log(newStack); // 4
+
+// const clubName = ['BARCELONA'];
+// const shortName = ['FCB'];
+// const teamName = shortName.concat(clubName);
+// console.log(teamName); // 5
+
+// const moto = ['More than a club'];
+// const  club = ['FCB'];
+// const whole = club.concat(moto);
+// console.log(whole); // 6
+
+// const moto = ['Hala Madrid'];
+// const club = ['RM'];
+// const clubBorad = club.concat(moto);
+// console.log(clubBorad); // 7
+
+// const richClub = ['PSG','Manchester City','Manchester United'];
+// const result = ['All are Loser'];
+// const allMoto = richClub. concat(result);
+// console.log(allMoto); // 8
+
+// const myName = ['Uchiha Sasuke'];
+// const moto= ['To kill a certain man'];
+// const clan= myName.concat(moto);
+// console.log(clan); // 9
+
+// const theLast = ['Boruto'];
+// const last = ['Naruto'];
+// const newARR = theLast.concat(last); // 10
