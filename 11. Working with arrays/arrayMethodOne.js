056df5def79e1c37c46b0e24a9c408c434f5b50e
Juwel-771 <juwelah99@gmail.com>
2022-05-31 18:49:50 +0600

    concate method

---
+++ 11. Working with arrays/arrayMethodOne.js
@@ -0,0 +1,13 @@
+'use strick'
+// Concat method.=> 15 times
+
+// const arr1 = [1,2,3,4];
+// const arr2 = [5,4,5,6];
+// const newArr = arr1.concat(arr2);
+// console.log(newArr); // 1
+
+// const arr1 = [1,2,3,4,5,6];
+// const arr2 = ['j','u','w','e','l'];
+// const newOBj = arr1.concat(arr2);
+// console.log(newOBj); // 2
+
