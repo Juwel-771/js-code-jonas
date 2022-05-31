9940f80eaa5de059b70982777a0a943a7fcdd659
Juwel-771 <juwelah99@gmail.com>
2022-05-31 12:11:03 +0600

    3rd challenge completed

--- 11. Working with arrays/challengeTwo.js
+++ 11. Working with arrays/challengeTwo.js
@@ -1,10 +1,18 @@
-'use strick'
+// 'use strick'
 const calcAverageHumanAge = function(ages){
     const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
     console.log(humanAges);
 
     const adults = humanAges.filter(age => age >= 18);
     console.log(adults);
+
+    const average = adults.reduce((acc, age) => acc + 
+    age, 0) / adults.length;
+    return average;
 };
 
-calcAverageHumanAge([5,2,4,1,15,8,3]);
+const avg1 = calcAverageHumanAge([5,2,4,1,15,8,3]);
+
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(avg1, avg2);
