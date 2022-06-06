0445015b5a35b3ba9f0502499045fe2c22790138
Juwel-771 <juwelah99@gmail.com>
2022-06-07 02:19:42 +0600

    Callback Function

---
+++ JS-ASYNC/callbackFunc.js
@@ -0,0 +1,11 @@
+function display(some){
+    console.log(some);
+}
+
+function calculation(num1, num2){
+    let result = num1 + num2;
+    return result;
+}
+
+let sum  = calculation(1,2);
+display(sum);
