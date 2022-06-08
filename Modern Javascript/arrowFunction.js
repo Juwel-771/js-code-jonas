6423a11fce5619dfdb4b57d9f49f841a348b2367
Juwel-771 <juwelah99@gmail.com>
2022-06-08 18:39:11 +0600

    Code Added

--- Modern Javascript/arrowFunction.js
+++ Modern Javascript/arrowFunction.js
@@ -1,5 +1,26 @@
-let number = () => {
-    return 10;
+// let number = () => {
+//     return 10;
+// }
+
+// console.log(number());
+
+// let sum = (a, b)=>{
+//     return a+b;
+// };
+
+// console.log("Total Add: " + sum(5,5));
+
+// let multi = (a, b) => a*b;
+// console.log(multi(6, 6));
+
+let javascript = {
+    name: "JavaScript",
+    libraries: ["React JS", "Angular Js","Vue.js"],
+    printLibraries: function(){
+        this.libraries.forEach((a) => {
+            console.log(`${this.name} love ${a}`);
+        })
+    }
 }
 
-console.log(number);
+javascript.printLibraries();
